dd8b46de5aa67234b3bd04835ea8382d26460166
= <=>
2022-05-17 20:37:51 +0700

    Test branch

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const express = require('express');
-const app  = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const userRoute = require('./routes/userRoutes')
-const authRoutes = require('./routes/authRoutes')
-const postRoutes = require("./routes/postRoutes")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
-    console.log('connected to MongoDb')
-})
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("connected to MongoDb");
+});
 
-
+//some changes
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
-
-
-app.use("/api/users", userRoute)
-app.use("/api/auth",authRoutes)
-app.use("/api/posts",postRoutes)
-
-app.listen(8800,()=>{
-    console.log("backend server is running")
-})
-
+app.listen(8800, () => {
+  console.log("backend server is running");
+});
